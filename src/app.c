aedbf6349240a05ce67840f43c40aaef1a03583c
ClementMab <mabileau.clement@gmail.com>
2021-09-28 23:19:33 +0200

    Partitioning of app.c

--- src/app.c
+++ src/app.c
@@ -1,43 +1,84 @@
 #include "app.h"
 
-int main(int argc,char* argv[]){
+void _init(SDL_Window** p_app_window, SDL_Renderer** p_renderer, char title[], int SCREEN_WIDTH, int SCREEN_HEIGHT){
     //SDL Init
     if(SDL_Init(SDL_INIT_VIDEO) < 0){
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "[debug] %s", SDL_GetError());
         return -1;
     }
 
-    const int SCREEN_WIDTH = 1980;
-    const int SCREEN_HEIGHT = 1080;
-
-    char title[] = "QuadTree Conway's Game Of Life";
-    SDL_Window* app_window = SDL_CreateWindow(title,
+    *p_app_window = SDL_CreateWindow(title,
     SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED
     ,SCREEN_WIDTH/2,SCREEN_WIDTH/2 ,
     SDL_WINDOW_SHOWN); //a window ratio 1:1
 
-    if (app_window == NULL){         
+    if (*p_app_window == NULL){         
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "[DEBUG] > %s", SDL_GetError());         
         SDL_Quit();         
         return EXIT_FAILURE;     
-    }  
+    }
 
-    SDL_Renderer* p_renderer= SDL_CreateRenderer(app_window,-1,SDL_RENDERER_ACCELERATED);
+    *p_renderer = SDL_CreateRenderer(*p_app_window,-1,SDL_RENDERER_ACCELERATED);
 
-    if (p_renderer == NULL){         
+    if (*p_renderer == NULL){         
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "[DEBUG] > %s", SDL_GetError());         
         SDL_Quit();
         return EXIT_FAILURE;     
     } 
+
+    return;
+}
+
+void update(){
+    return;
+}
+
+void _draw(SDL_Window* app_window, SDL_Renderer* renderer, world W){
+    //clear the screen:
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderClear(renderer);
     
+    //cells drawing
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    int W_WIDTH = -1;
+    int W_HEIGHT = -1;
+    SDL_GetWindowSize(app_window,&W_WIDTH,&W_HEIGHT);
+    render_universe(W,renderer,W_WIDTH,W_HEIGHT);
+
+    //test
+    SDL_Rect rect = {100,100,50,50};
+    SDL_RenderFillRect(renderer,&rect);
+
+    return;
+}
+
+void _quit(SDL_Window* app_window, SDL_Renderer* renderer){
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(app_window);
+    SDL_Quit();
+    return;
+}
+
+int main(int argc, char* argv[]){
+
+    const int SCREEN_WIDTH = 1980;
+    const int SCREEN_HEIGHT = 1080;
+
+    char title[] = "QuadTree Conway's Game Of Life";
+
+    SDL_Window* app_window;  //we neglect the pointer nature of window and renderer in the namae
+    SDL_Renderer* renderer;
     SDL_Event events;
 
-    bool isOpen = true;
+    _init(&app_window,&renderer,title,SCREEN_WIDTH,SCREEN_HEIGHT);
 
     //Universe Init:
     world W = {new_cell(2,0,0,false),{0,0,3,3}};
     W.root->children = make_children(*W.root);
     W.root->children[3]->alive = true; //diff from W in test-universe
+
+    //gameLoop:
+    bool isOpen = true;
 
     while (isOpen){
         while (SDL_PollEvent(&events)){
@@ -51,37 +92,18 @@ int main(int argc,char* argv[]){
                     break;
             }
         }
+        update();
+
+        _draw(app_window, renderer, W);
+
         /*
-        update the univ
-        */
-
-        //clear the screen:
-        SDL_SetRenderDrawColor(p_renderer, 255, 255, 255, 255);
-        SDL_RenderClear(p_renderer);
-        
-        //cells drawing
-        SDL_SetRenderDrawColor(p_renderer, 0, 0, 0, 255);
-        int W_WIDTH = -1;
-        int W_HEIGHT = -1;
-        SDL_GetWindowSize(app_window,&W_WIDTH,&W_HEIGHT);
-        render_universe(W,p_renderer,W_WIDTH,W_HEIGHT);
-
-        //test
-        SDL_Rect rect = {100,100,50,50};
-        SDL_RenderFillRect(p_renderer,&rect);
-
-        
-        /*
-        GUI: step
+        GUI: 
         */       
 
-        SDL_RenderPresent(p_renderer);
+        //displaying:
+        SDL_RenderPresent(renderer);
     }
 
-
-
-    SDL_DestroyRenderer(p_renderer);
-    SDL_DestroyWindow(app_window);
-    SDL_Quit();
+    _quit(app_window,renderer);
     return EXIT_SUCCESS;
 }
--- tests/random_tests.c
+++ tests/random_tests.c
@@ -28,6 +28,12 @@ void arrays_in_struct(struct_test t1){
     printf("[%d,%d] at %p\n",t1.arr[0][0],t1.arr[1][0],t1.arr);
 }
 
+void test_pointer(int* p){
+    //printf("%d\n",*p); --> seg fault sometimes
+    *p = 5;
+    printf("%d\n",*p);
+}
+
 int main(){/*
     struct_test test = {0,{{1},{2}}};
     printf("%d, [%d, %d]\n",test.x,test.arr[0][0],test.arr[1][0]); 
@@ -46,10 +52,15 @@ int main(){/*
     printf("%d at %p\n",t1.x,&t1);
     value_vs_pointer(t1);
 
-    printf("Proves the cal_by_value for struct.\nBut what for arrays in struct ?\n");
+    printf("Proves the call_by_value for struct.\nBut what for arrays in struct ?\n");
 
     printf("[%d,%d] at %p\n",t1.arr[0][0],t1.arr[1][0],t1.arr);
 
     arrays_in_struct(t1);
+
+    int p = -1;
+
+    test_pointer(&p);
     return 0;
 }
+
